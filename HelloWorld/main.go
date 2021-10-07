e8121ea83c374fa8d9f069a77a0a1a7c13da1e2c
Anilkumar Manaparampil <ankumar@m-c02fd44fml85.paloaltonetworks.local>
2021-10-07 16:43:38 -0500

    Hello World

---
+++ HelloWorld/main.go
@@ -0,0 +1,7 @@
+package main // Use of word main would mean it is an executable package.
+
+import "fmt" //Used to give our package access to some code written in another package. Gives access to fmt library
+
+func main() { //func is nothing but short for function. You can pass arguments in the () brackets
+	fmt.Println("Hi There")
+}
